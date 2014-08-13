415b6984eb8c2518ccdcb123296435dbd927b401
Erol Can OZGEN <erolcanozgen@gmail.com>
2014-08-13 14:37:24 +0300

    C# projesi eklendi. InsertionSort gerçeklendi.

---
+++ AlgorithmsC#/Sorting/InsertionSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sorting
+{
+    class InsertionSort
+    {
+        static public void insertionSort<T>(List<T> data)
+        {
+            for (int i = 1; i < data.Count;i++)
+            {
+                T tmp = data[i];
+                for (int j = i-1; j >= 0; j--)
+                {
+                    if (Comparer<T>.Default.Compare(tmp, data[j]) < 0)
+                    {
+                        data[j+1] = data[j];
+                        data[j] = tmp;
+                    }
+                }
+            }
+        }
+
+        static public void insertionSort<T>(T[] data)
+        {
+            for (int i = 1; i < data.Length; i++)
+            {
+                T tmp = data[i];
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (Comparer<T>.Default.Compare(tmp, data[j]) < 0)
+                    {
+                        data[j + 1] = data[j];
+                        data[j] = tmp;
+                    }
+                }
+            }
+        }
+
+
+    }
+}
---
+++ AlgorithmsC#/Sorting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> asd = new List<int>();
+            Random rd = new Random();
+            for (int i = 0; i < 10000; i++)
+            {
+                int a = rd.Next(0, 1000000); 
+                asd.Add(a);
+            }
+                //InsertionSort.insertionSort(asd);
+            int[] adf = asd.ToArray();
+            InsertionSort.insertionSort(adf);
+        }
+    
+    }
+
+}
